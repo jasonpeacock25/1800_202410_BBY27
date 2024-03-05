db3f10e19613b0b7d7f730b12283f529943d90b1
ZhaoqiuD <dingzq0807@gmail.com>
2024-03-04 22:14:11 -0800

    Develop user login & authentication; worked on navbar

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@ function sayHello() {
     
 }
 //sayHello();
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
+}
