2cdf12aab33a34f12b03cf594324c0e783cc4e04
Seohyeon Park <chris8663@naver.com>
2024-03-20 14:34:16 -0700

    modified gitignore

---
+++ scripts/firebaseAPI_BBY27.js
@@ -0,0 +1,20 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX4wFwGsF-R5ZxNVg7fysU-XTyUmTGlfw",
+  authDomain: "comp1800-202410-bby27.firebaseapp.com",
+  projectId: "comp1800-202410-bby27",
+  storageBucket: "comp1800-202410-bby27.appspot.com",
+  messagingSenderId: "590374870861",
+  appId: "1:590374870861:web:0a0b524c7dfb80c6826104"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
