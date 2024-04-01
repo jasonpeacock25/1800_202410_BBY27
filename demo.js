da38635475e37cd429fd3a50c31893286b13e624
Seohyeon Park <chris8663@naver.com>
2024-04-01 15:51:57 -0700

    added demo files to connect json and js

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
 const table = document.querySelector('#time-table');
+const daysCol = document.querySelector('#days');
 const classes = [];
+
+const times = [830, 930, 1030, 1130, 1230, 1330, 1430, 1530, 1630, "night"];
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
 function retrieveSchedule() {
     fetch("./default-schedule.json").then(res => {
         if (res.ok) {
@@ -9,36 +14,65 @@ function retrieveSchedule() {
         }
 
     }).then(data => {
+        // console.log(data);
         data.forEach(each => { // monday tuesday .. 
+            console.log(each);
             Object.keys(each).forEach(key => {
-                console.log(`${key} : ${each[key]}`);
                 classes.push(each[key])
             })
-            
+
         })
-        console.log(classes);
+        // console.log(classes);
+        populateTime(times)
+        populateData()
+
     })
 
 }
 retrieveSchedule()
-const times = [830, 930, 1030, 1130, 1230, 1330, 1430, 1530, 1630, "night"];
+
 
 function populateTime(times) {
     let div = `<div class="table"></div>` // empty slot
 
-    console.log(times.length);
 
-    for(let i = 0; i < times.length; i++) {
-        console.log(times[i]);
-        div += `<div class="table">${times[i]}</div>`
+    for (let i = 0; i < times.length; i++) {
+        div += `<div class="table times">${times[i]}</div>`
     }
     table.innerHTML += div
 }
 
-populateTime(times)
 
-function populateData(key, value) {
-    populateTime(key)
+
+
+
+function populateData(){
+
+    for (let j = 0; j < classes.length; j++) {
+        let div = document.createElement('div');
+        div.classList.add('table');
+
+        let data = classes[j];
+
+        div.innerText = data; // if innerText value is null, nothing will appear 
+        if(data) {
+            div.classList.add('school');
+        }
+        
+
+        table.appendChild(div);
+    }
 }
 
 
+
+function populateDays() {
+    let div = ""
+    days.forEach(day => {
+        div += `<div class="table">${day}</div>`
+    })
+    console.log(div);
+    daysCol.innerHTML = div
+    // console.log("ddf");
+}
+populateDays()
